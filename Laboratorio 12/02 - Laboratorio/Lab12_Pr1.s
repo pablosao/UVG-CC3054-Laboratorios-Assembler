adfc72ee114b87fbae607c0ed7efaec0488bc934
Pablo Sao <pablo_sao@outlook.com>
2019-05-05 10:18:05 -0600

    subiendo primer programa, se realizo solo lectura e identificación de caracter

---
+++ Laboratorio 12/02 - Laboratorio/Lab12_Pr1.s
@@ -0,0 +1,71 @@
+
+/**********************************************************************/
+/*         Autor: Pablo Sao                                           */
+/*         Fecha: 05 de mayo de 2019                                  */
+/*   Descripcion: Si ingresa un número, enciente led en GPIO 17       */ 
+/*                Si es un caracter, enciente led en GPIO 22          */
+/**********************************************************************/
+
+.text
+.align 2
+.global main
+.type main, %function
+
+main:
+	STMFD SP!, {LR}
+	
+	LDR   R0, =msjIngreso 			@ dirección de mensaje
+	BL    puts
+
+	@** Lectura de  teclado, mediante Interrupcion de OS
+
+	MOV   R7, #3					@ codigo de interrupcion para lectura
+	MOV   R0, #0					@ indicando que es en monitor
+	MOV   R2, #1					@ solo leeremos 1 caracter
+	MOV   R1, =dataIn
+	SWI   0
+
+	LDR   R1, =dataIn
+	LDRB  R1, [R1]
+	
+	CMP   R1, #0
+	CMPGE R1, #9
+	BLLE  _esNumero
+	
+	B     _esCaracter
+	
+_esNumero:
+	LDR   R0, =displayNum
+	BL    printf
+
+	BL    _exit
+
+_esCaracter:
+	LDR   R0, =displayCh
+	BL    printf
+
+	BL    _exit
+
+_exit:
+	LDMFD SP!,{LR}
+	MOV   R7, #1
+	SWI   0
+	BX    LR
+	
+.data
+
+.align 2
+msjIngreso:
+	.ascii "Ingrese 1 caracter (numerico o letra): "
+
+.align 2
+dataIn:
+	.ascii " "
+
+.align 2
+displayNum:
+	.asciz "Número: %d\n"
+
+.align 2
+displayCh:
+	.asciz "Caracter: %s\n"
