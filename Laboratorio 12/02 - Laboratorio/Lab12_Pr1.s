b9252b520d9e6da5cf0fb9efbf6aca849cb5ce65
Pablo Sao <pablo_sao@outlook.com>
2019-05-05 10:57:49 -0600

    se agrega tabla y se compone primer programa para identificación de numero y caracter

--- Laboratorio 12/02 - Laboratorio/Lab12_Pr1.s
+++ Laboratorio 12/02 - Laboratorio/Lab12_Pr1.s
@@ -2,7 +2,7 @@
 /**********************************************************************/
 /*         Autor: Pablo Sao                                           */
 /*         Fecha: 05 de mayo de 2019                                  */
-/*   Descripcion: Si ingresa un número, enciente led en GPIO 17       */ 
+/*   Descripcion: Si ingresa un número, enciente led en GPIO 17       */
 /*                Si es un caracter, enciente led en GPIO 22          */
 /**********************************************************************/
 
@@ -13,27 +13,28 @@
 
 main:
 	STMFD SP!, {LR}
-	
+
 	LDR   R0, =msjIngreso 			@ dirección de mensaje
 	BL    puts
 
 	@** Lectura de  teclado, mediante Interrupcion de OS
 
-	MOV   R7, #3					@ codigo de interrupcion para lectura
-	MOV   R0, #0					@ indicando que es en monitor
-	MOV   R2, #1					@ solo leeremos 1 caracter
-	MOV   R1, =dataIn
+	MOV   R7, #3				@ codigo de interrupcion para lectura
+	MOV   R0, #0				@ indicando que es en monitor
+	MOV   R2, #1				@ solo leeremos 1 caracter
+	LDR   R1, =dataIn
 	SWI   0
 
 	LDR   R1, =dataIn
-	LDRB  R1, [R1]
-	
-	CMP   R1, #0
-	CMPGE R1, #9
+	LDRB   R1, [R1]
+
+	CMP   R1, #0x30				@ Si R1 >= 0
+	CMPGE R1, #0x39				@    & R1 <= 9
 	BLLE  _esNumero
-	
+
 	B     _esCaracter
 	
+
 _esNumero:
 	LDR   R0, =displayNum
 	BL    printf
@@ -51,21 +52,21 @@ _exit:
 	MOV   R7, #1
 	SWI   0
 	BX    LR
-	
+
 .data
 
 .align 2
 msjIngreso:
-	.ascii "Ingrese 1 caracter (numerico o letra): "
+	.ascii "Ingrese 1 caracter (número o letra): "
 
 .align 2
 dataIn:
-	.ascii " "
+	.ascii ""
 
 .align 2
 displayNum:
-	.asciz "Número: %d\n"
+	.asciz "Número: %c\n"
 
 .align 2
 displayCh:
-	.asciz "Caracter: %s\n"
+	.asciz "Caracter: %c\n"
